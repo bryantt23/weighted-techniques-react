237414dd9f7a83265dd7909eb39d0fe244d44638
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-05-26 01:22:24 -0700

    handle like

--- src/components/Technique.jsx
+++ src/components/Technique.jsx
@@ -1,18 +1,29 @@
 import { useState, forwardRef } from 'react'
+import { handleLike } from '../services/techniquesApi'
 
 const HIGHLIGHTED_CLASS_NAME = "current";
 
 const Technique = forwardRef(({ technique }, ref) => {
   const [showDescription, setShowDescription] = useState(false)
+  const [isLiked, setIsLiked] = useState(false)
   const { name, weight, description, _id, isHighlighted } = technique
 
   const toggleDescription = () => {
     setShowDescription(prev => !prev)
   }
 
-  const handleLikeClick = (e) => {
+  const handleLikeClick = async (e) => {
     e.stopPropagation();
     console.log('Like button clicked for:', _id);
+    try {
+      await handleLike(_id)
+      setIsLiked(true)
+      setTimeout(() => {
+        setIsLiked(false)
+      }, 2000)
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (<li
@@ -23,7 +34,12 @@ const Technique = forwardRef(({ technique }, ref) => {
       {name} ({weight})
     </div>
     {(description && showDescription) && <p className="description">{description}</p>}
-    <button className="like-btn" onClick={handleLikeClick}>Like</button>
+    <button
+      className={`like-btn ${isLiked && "liked"}`}
+      disabled={isLiked}
+      onClick={handleLikeClick}>
+      {isLiked ? "Like 👍" : "Like"}
+    </button>
     {description && <button className="toggle-btn toggle" >Show Description</button>}
   </li>)
 })
--- src/services/techniquesApi.js
+++ src/services/techniquesApi.js
@@ -11,3 +11,18 @@ export async function getTechniques() {
         throw error;  // Re-throw to handle it in the main logic or another helper
     }
 }
+
+export async function handleLike(id) {
+    try {
+        const response = await axios(`https://thought-techniques-api-git-main-bryantt23s-projects.vercel.app/techniques/${id}`, {
+            method: 'PATCH',  // Specify the method
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        const { data } = response
+        console.log("Data updated from API:", data);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
