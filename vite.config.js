c0a92181dd3510ab89422f8b9b82bfc288fd8c23
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-05-28 05:13:53 -0700

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/weighted-techniques-react/',
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Thought Smasher',
+        short_name: 'ThoughtSmasher',
+        description: 'My Vite React app as a PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true, // Enable PWA support in development
+      },
+    }),
+  ],
+});
