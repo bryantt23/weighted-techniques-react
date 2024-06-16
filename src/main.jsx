0d90675ea5f7a4ce0fd265eb6ff6104eaa6b51c6
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-06-16 20:20:53 +0700

    fix bug, service file was not found at github pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(registration => {
+    navigator.serviceWorker.register('/weighted-techniques-react/sw.js', { scope: '/weighted-techniques-react/' }).then(registration => {
       console.log('SW registered: ', registration);
     }).catch(registrationError => {
       console.log('SW registration failed: ', registrationError);
--- vite.config.js
+++ vite.config.js
@@ -13,24 +13,31 @@ export default defineConfig({
         short_name: 'ThoughtSmasher',
         description: 'My Vite React app as a PWA',
         theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/weighted-techniques-react/',
+        start_url: '/weighted-techniques-react/',
         icons: [
           {
             src: '/weighted-techniques-react/android-chrome-192x192.png',
             sizes: '192x192',
-            type: 'image/png',
+            type: 'image/png'
+          },
+          {
+            src: '/weighted-techniques-react/android-chrome-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
           },
           {
             src: '/weighted-techniques-react/android-chrome-256x256.png',
             sizes: '256x256',
             type: 'image/png',
-          },
-          {
-            src: '/weighted-techniques-react/android-chrome-256x256.png',
-            sizes: '256x256',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,webmanifest}']
       },
       devOptions: {
         enabled: true, // Enable PWA support in development
