e0bf23874c4fa440be952236c5ea0fc2a67a40e3
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-06-16 20:01:07 +0700

    Updates

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/weighted-techniques-react/sw.js', { scope: '/weighted-techniques-react/' })})}
