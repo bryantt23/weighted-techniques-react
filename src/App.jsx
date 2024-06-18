c9993f804910f48d36fa146f189cfaa0685e2ec2
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-06-18 20:37:41 +0700

    add placeholder for techniques raw page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import Techniques from './components/Techniques'
-import './App.css'
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import Techniques from './components/Techniques';
+import TechniquesListRaw from './components/TechniquesListRaw';
 
 function App() {
-
   return (
-    <>
-      <Techniques />
-    </>
-  )
+    <Router basename="/weighted-techniques-react">
+      <Routes>
+        <Route path="/" element={<Techniques />} />
+        <Route path="/raw" element={<TechniquesListRaw />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TechniquesListRaw.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TechniquesListRaw() {
+    return (
+        <div>
+            TechniquesListRaw
+        </div>
+    );
+}
+
+export default TechniquesListRaw;
